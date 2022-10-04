68c1f4ca75f88007540e2468e7beacccf37dfdc4
thiagowaib <thiagowaib@gmail.com>
2022-10-04 14:30:07 +0000

    IaC - Servidor Web

---
+++ IaC2-Servidor_Web/iac2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo ""
+echo "Atualizando o sistema..."
+apt update
+apt upgrade -y
+
+echo ""
+echo "Instalando pacotes..."
+apt install  apache2 -y
+apt install unzip -y
+
+echo ""
+echo "Baixando aplicacao..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo ""
+echo "Extraindo arquivos..."
+unzip main.zip
+
+echo ""
+echo "Copiando arquivos..."
+cd linux-site-dio
+cp -R * /var/www/html
+
+echo ""
+echo "Finalizado."
+echo ""
