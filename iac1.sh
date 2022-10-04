a61e142eb21901287cc9018679acaf9f884e8e04
thiagowaib <thiagowaib@gmail.com>
2022-10-04 11:55:52 +0000

    IaC Bash File

---
+++ iac1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo ""
+echo "Criando diretórios..."
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo ""
+echo "Criando grupos de usuários..."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo ""
+echo "Criando usuários..."
+
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_ADM
+
+useradd debora -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_VEN
+
+useradd josefina -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd senha123) -G GRP_SEC
+
+echo ""
+echo "Especificando permissões..."
+
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
+
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
+
+echo ""
+echo "Script finalizado."
+echo ""
